2e5784a82753ddb1de420b5d8d040ad3a667e36f
armarchm <armarchm@uwaterloo.ca>
2016-11-30 22:52:04 -0500

    Enhance!

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function init() {
-	var r= $('<input type="button" value="Red" onclick="red()"/>');
-	$("body").append(r);
+	$("body").append($('<input type="button" value="Red" class="ui-btn ui-btn-b" onclick="red()"/>'));
+	$("body").append($('<input type="button" value="Green" class="ui-btn ui-btn-b" onclick="red()"/>'));
+	$("body").append($('<input type="button" value="Blue" class="ui-btn ui-btn-b" onclick="red()"/>'));
+	$("body").append($('<input type="button" value="White" class="ui-btn ui-btn-b" onclick="red()"/>'));
+	$("body").append($('<input type="button" value="Fade" class="ui-btn ui-btn-b" onclick="red()"/>'));
 }
 
 
