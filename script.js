79d08910bd162b654feb434a50fc75d60c0fa8c6
armarchm <armarchm@uwaterloo.ca>
2016-11-30 22:45:50 -0500

    Enhance

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function test(){
-	alert("Hello!");
+function init() {
+	var r= $('<input type="button" value="Red" onclick="red()"/>');
+	$("body").append(r);
+}
+
+
+function red() {
+	alert("Switch to red!");
 }
